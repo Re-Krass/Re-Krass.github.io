cdeadae766c6e481b5c0d6a84424e6d8e0566f47
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2019-08-28 16:36:24 +0200

    refactor(buildsite): add linebreaks

--- re-krass-website/buildsite.ps1
+++ re-krass-website/buildsite.ps1
@@ -1,11 +1,15 @@
 ﻿$loc = Get-Location;
 Write-Host -ForegroundColor Yellow "Build site - script";
+
 Write-Host -ForegroundColor Yellow "1: Run lint";
 pnpm run lint;
+
 Write-Host -ForegroundColor Yellow "2: Run build";
 pnpm run build;
+
 Write-Host -ForegroundColor Yellow "3: Generate documentation";
 vuese gen;
+
 Write-Host -ForegroundColor Yellow "4: Apply dark-theme";
 # get the current folder
 $currentFolder = Split-Path $MyInvocation.MyCommand.Path
@@ -16,5 +20,6 @@ Write-Host -ForegroundColor Yellow "5: Move website elements to destination";
 robocopy /S /E .\website-build .\ /move;
 # git add .;
 # cd ./re-krass-website;
+
 cd $loc;
 Write-Host -ForegroundColor Yellow "Script finished";
